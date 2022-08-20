c5fa8f3e3dc6a7796d5cec52d8e03b6eb307075c
SANGWOO-JOO <turncoat19@gmail.com>
2022-08-20 23:07:41 +0900

    spring

---
+++ 스프링 웹개발/PART5/b01/b01/src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -0,0 +1,35 @@
+package org.zerock.b01.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor//파라미터가 없는 기본 생성자를 생성
+
+public class BoardDTO {
+
+    private Long bno;
+
+    @NotEmpty
+    @Size(min=3, max=100)
+    private String title;
+
+    @NotEmpty
+    private String content;
+
+    @NotEmpty
+    private String writer;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime modDate;
+}
